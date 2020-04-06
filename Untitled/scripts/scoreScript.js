4544403e2d0072be1a56df5b430fe6f087fa3db5
Al-Fatih Art <restu6005@gmail.com>
2020-04-06 12:23:14 +0800

    Enjoy the game!

---
+++ Untitled/scripts/scoreScript.js
@@ -0,0 +1,37 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene');
+const Patches = require('Patches');
+
+// Use export keyword to make a symbol available in scripting debug console
+export const Diagnostics = require('Diagnostics');
+
+// To use variables and functions across files, use export/import keyword
+// export const animationDuration = 10;
+
+// Use import keyword to import a symbol from another file
+// import { animationDuration } from './script.js'
+
+// To access scene objects
+const directionalLight = Scene.root.find('directionalLight0');
+var counterNumber = Scene.root.find('number');
+var scoreNumber = Patches.getScalarValue('score');
+
+// To access class properties
+const directionalLightIntensity = directionalLight.intensity;
+counterNumber.text = scoreNumber.toString();
+
+// To log messages to the console
+// Diagnostics.log('Console message logged from the script.');
